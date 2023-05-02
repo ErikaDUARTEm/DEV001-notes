9fceeff05855feab07a95e698c6f8743b99bbb69
Erika Duarte <erika.duarte.mora@gmail.com>
2023-05-01 19:14:14 -0500

    instalacion y configuracion de tailwind

---
+++ my-organized-notes/src/components/Login.jsx
@@ -0,0 +1,21 @@
+
+
+export default function Login() {
+  return (
+    <>
+        <form>
+            <label htmlFor="email">
+                Introduce tu email: 
+                <input type='email'/>
+            </label>
+            <label htmlFor="password">
+                Introduce tu contraseña: 
+                <input type='password'/>
+            </label>
+        </form>
+
+
+
+    </>
+  )
+}
